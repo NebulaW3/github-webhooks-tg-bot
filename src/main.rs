6bbc86a240281e64e8a8a79038deb4067d0b0ff3
Pavel Paranin <pavel@paran.in>
2023-07-21 23:41:58 +0200

    message debug

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,11 @@ struct AppState {
 #[post("/process-webhook")]
 async fn process_webhook(payload: String, data: web::Data<AppState>) -> impl Responder {
     println!("{}", payload);
-    data.bot
-        .send_message(ChatId(502462376), payload.clone())
-        .await
-        .unwrap();
+    let res = data
+        .bot
+        .send_message(ChatId(502462376), &payload[..1000])
+        .await;
+    dbg!("{}", res);
     payload
 }
 
