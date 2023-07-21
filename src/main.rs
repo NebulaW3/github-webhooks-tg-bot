887e513cefc7b848c9c4e55f328221865f532164
Pavel Paranin <pavel@paran.in>
2023-07-21 15:20:11 +0200

    Teloxide bot is added

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
+use teloxide::prelude::*;
 
-#[post("/process-webhook")]
-async fn process_webhook(payload: String) -> impl Responder {
-    println!("{}", payload);
-    payload
+struct AppState {
+    bot: Bot,
 }
 
+#[post("/process-webhook")]
+async fn process_webhook(payload: String, data: web::Data<AppState>) -> impl Responder {
+    println!("{}", payload);
+    data.bot.send_message(ChatId(502462376), payload.clone());
+    payload
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .app_data(web::Data::new(AppState {
+                bot: Bot::from_env(),
+            }))
             .service(process_webhook)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
-
