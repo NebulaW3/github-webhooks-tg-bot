806187129677719196e477edfb39fd2e5ea2a15a
Pavel Paranin <pavel@paran.in>
2023-07-22 16:11:49 +0200

    Serde added

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 use actix_web::{post, web, App, HttpServer, Responder};
-use teloxide::{payloads, prelude::*, types::Recipient};
+use serde_json::{Result, Value};
+use teloxide::{
+    payloads,
+    prelude::*,
+    types::{ParseMode, Recipient},
+};
 
 struct AppState {
     bot: Bot,
 }
 
-#[post("/process-webhook")]
+#[post("/issues")]
 async fn process_webhook(payload: String, data: web::Data<AppState>) -> impl Responder {
-    println!("{}", payload);
-    let _res = data
-        .bot
-        .send_message_to_thread(ChatId(-1001986164831), 13915, &payload[..1000])
+    let webhook_data: Value = serde_json::from_str(&payload.as_str()).unwrap();
+    let text = format!(
+        "Issue is <b>{}</b>, for more details see {}",
+        webhook_data["action"].as_str().unwrap_or(""),
+        webhook_data["issue"]["html_url"].as_str().unwrap_or("")
+    );
+    data.bot
+        .send_message_to_thread(ChatId(-1001986164831), 13915, text)
         .await;
     payload
 }
@@ -49,7 +58,9 @@ impl SendToThread for Bot {
     {
         Self::SendMessage::new(
             self.clone(),
-            payloads::SendMessage::new(chat_id, text).message_thread_id(thread_id),
+            payloads::SendMessage::new(chat_id, text)
+                .message_thread_id(thread_id)
+                .parse_mode(ParseMode::Html),
         )
     }
 }
