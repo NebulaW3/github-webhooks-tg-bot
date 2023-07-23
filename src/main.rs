5b12e4ba5b2902227bcf99c3b7282a1bae03af36
Pavel Paranin <pavel@paran.in>
2023-07-23 13:47:32 +0200

    PR and PR review endpoints are added

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ struct AppState {
     bot: Bot,
 }
 
-async fn bot_send_message(bot: &Bot, text: String) -> <Bot as Requester>::SendMessage {
+fn bot_send_message(bot: &Bot, text: String) -> <Bot as Requester>::SendMessage {
     bot.send_message_to_thread(ChatId(CHAT_ID), THREAD_ID, text)
 }
 
@@ -25,19 +25,71 @@ async fn process_issue(payload: String, data: web::Data<AppState>) -> impl Respo
         webhook_data["action"].as_str().unwrap_or(""),
         webhook_data["issue"]["html_url"].as_str().unwrap_or("")
     );
+
+    bot_send_message(&data.bot, text).await;
+    ""
+}
+
+#[post("/pr")]
+async fn process_pr(payload: String, data: web::Data<AppState>) -> impl Responder {
+    let webhook_data: Value = serde_json::from_str(&payload.as_str()).unwrap();
+    let text =
+        format!(
+        "<a href='{}'>Pull request</a> for <a href='{}'>{}</a> was <b>{}</b> by <a href='{}'>{}</a>.",
+        webhook_data["pull_request"]["html_url"].as_str().unwrap_or(""),
+        webhook_data["repository"]["html_url"]
+            .as_str()
+            .unwrap_or(""),
+        webhook_data["repository"]["full_name"]
+            .as_str()
+            .unwrap_or(""),
+        webhook_data["pull_request"]["user"]["html_url"].as_str().unwrap_or(""),
+        webhook_data["pull_request"]["user"]["login"]
+            .as_str()
+            .unwrap_or(""),
+        webhook_data["action"].as_str().unwrap_or(""),
+    );
+
+    bot_send_message(&data.bot, text).await;
+    ""
+}
+
+#[post("/pr-review")]
+async fn process_pr_review(payload: String, data: web::Data<AppState>) -> impl Responder {
+    let webhook_data: Value = serde_json::from_str(&payload.as_str()).unwrap();
+    let text = format!(
+        "<a href='{}'>Review </a> for <a href='{}'>PR</a> was <b>{}</b> by <a href='{}'>{}</a>.\n\n<i>{}</i>",
+        webhook_data["review"]["html_url"].as_str().unwrap_or(""),
+        webhook_data["pull_request"]["html_url"]
+            .as_str()
+            .unwrap_or(""),
+        webhook_data["action"].as_str().unwrap_or(""),
+        webhook_data["review"]["user"]["html_url"].as_str().unwrap_or(""),
+        webhook_data["review"]["user"]["login"]
+            .as_str()
+            .unwrap_or(""),
+        webhook_data["review"]["body"].as_str().unwrap_or(""),
+    );
+
     bot_send_message(&data.bot, text).await;
     ""
 }
 
 #[post("/pr-review-comment")]
 async fn process_pr_review_comment(payload: String, data: web::Data<AppState>) -> impl Responder {
-    let webhook_data: Value = serde_json::from_str(&payload.as_str()).unwrap();
+    let webhook_data: Value = serde_json::from_str(payload.as_str()).unwrap();
     let text = format!(
-        "New review comment for PR: {}\n\n<i>{}</i>\n\nBy {}\n\n{}",
-        webhook_data["pull_request"]["html_url"],
-        webhook_data["comment"]["body"],
-        webhook_data["comment"]["user"]["name"],
-        webhook_data["comment"]["html_url"],
+        "<a href='{}'>Review comment</a> for <a href='{}'>PR</a> was <b>{}</b> by <a href='{}'>{}</a>.\n\n<i>{}</i>",
+        webhook_data["comment"]["html_url"].as_str().unwrap_or(""),
+        webhook_data["pull_request"]["html_url"]
+            .as_str()
+            .unwrap_or(""),
+        webhook_data["action"].as_str().unwrap_or(""),
+        webhook_data["comment"]["user"]["html_url"].as_str().unwrap_or(""),
+        webhook_data["comment"]["user"]["login"]
+            .as_str()
+            .unwrap_or(""),
+        webhook_data["comment"]["body"].as_str().unwrap_or(""),
     );
 
     bot_send_message(&data.bot, text).await;
@@ -52,6 +104,8 @@ async fn main() -> std::io::Result<()> {
                 bot: Bot::from_env(),
             }))
             .service(process_issue)
+            .service(process_pr)
+            .service(process_pr_review)
             .service(process_pr_review_comment)
     })
     .bind(("0.0.0.0", 8080))?
